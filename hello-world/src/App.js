50081b476ddd213092bc60068ab3348c69468bb9
ChasersX <dustin.lafley@gmail.com>
2021-08-16 15:09:24 -0500

    Addednew js files

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <Welcome /> */}
+      <Message />
+      <Greet name = 'Bruce' heroName = 'Batman'>
+        <p>This is Children props</p>
+      </Greet>
+      <Greet name = 'Peter' heroName = 'Spider-Man'>
+        <button>Acitve</button>
+      </Greet>
+      <Greet name = 'Dr' heroName = 'Doc Oct'/>
+
+      <Welcome name = 'Bruce' heroName = 'Batman'>
+        <p>This is Children props</p>
+      </Welcome>
+      <Welcome name = 'Peter' heroName = 'Spider-Man'>
+        <button>Acitve</button>
+      </Welcome>
+      <Welcome name = 'Dr' heroName = 'Doc Oct'/>
       <Hello />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
